d6ff0dfc0d7f2d03fa615142e9919281a1833566
Tianhao Zhou <zhoutianhao03@meituan.com>
2025-10-19 15:01:08 -0700

    chore(android): update app package name to com.hejitech.time_since

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.time_since"
+    namespace = "com.hejitech.time_since"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.time_since"
+        applicationId = "com.hejitech.time_since"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
