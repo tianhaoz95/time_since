000d0b7d05993f018bde1b7d7d635d55a10b584f
Tianhao Zhou <zhoutianhao03@meituan.com>
2025-10-20 11:44:44 -0700

    Revert "feat(auth): add Firebase authentication with email/password and GoogleService-Info.plist configuration"
    This reverts commit 131593e68805fcb2a522fe4c466e7e0435106337.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,8 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
 }
