852d798f38dda5f0b03a39cf3f8dcb65249fbbd3
Tianhao Zhou <zhoutianhao03@meituan.com>
2025-10-29 21:42:24 -0700

    fix: hide debug banner in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Maintenance Tracker',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: StreamBuilder<User?>(
